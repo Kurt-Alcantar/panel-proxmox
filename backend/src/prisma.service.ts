e03934b0764191a218d40189482d3fc216f8cb7a
Kurt-Alcantar <kalcantar@hyperox.biz>
2026-04-06 12:16:17 -0600

    update

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -4,6 +4,18 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    let retries = 10;
+
+    while (retries) {
+      try {
+        await this.$connect();
+        console.log('✅ Connected to DB');
+        break;
+      } catch (err) {
+        console.log('⏳ Waiting for DB...');
+        await new Promise(res => setTimeout(res, 3000));
+        retries--;
+      }
+    }
   }
 }

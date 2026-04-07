39dbc470837c65b8c0e16443b44df94c9bc155a5
Kurt-Alcantar <kalcantar@hyperox.biz>
2026-04-07 14:52:23 -0600

    update

--- backend/src/auth.controller.ts
+++ backend/src/auth.controller.ts
@@ -24,4 +24,18 @@ export class AuthController {
 
     return response.data;
   }
+  @Post('refresh')
+  async refresh(@Body() body: any) {
+    const params = new URLSearchParams()
+    params.append('client_id', 'admin-cli')
+    params.append('grant_type', 'refresh_token')
+    params.append('refresh_token', body.refresh_token)
+
+    const response = await axios.post(
+      'http://keycloak:8080/realms/master/protocol/openid-connect/token',
+      params,
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    )
+    return response.data
+  }
 }
